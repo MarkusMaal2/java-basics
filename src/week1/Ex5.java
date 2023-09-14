6c9c5565a9b494ea7b96d93c73c5a9feefd0218d
MarkusMaal2 <markus.maal@voco.ee>
2023-09-14 13:10:35 +0300

    Week 1: Exercise 5

---
+++ src/week1/Ex5.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        int days = 365;
+        int onedayseconds = 24 * 60 * 60;
+        int seconds = onedayseconds * days;
+        System.out.println("There are " + seconds + " seconds in a year");
+    }
+}
